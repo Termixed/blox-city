9a115524644ac2a10d9901e254d7cb26d81adb6f
Termixed <87498719+Termixed@users.noreply.github.com>
2022-03-16 12:29:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <body>
     <form action="" method="POST">
       <input type="text" name="username" placeholder="Enter your username...">
-      <button type="submit"></button>
+      <button type="button" name="submit"></button>
     </form>
   </body>
 </html>
