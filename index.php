cf6830effa1621a76ccecf5480e3d3aa77dcb85e
Termixed <87498719+Termixed@users.noreply.github.com>
2022-03-16 12:28:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
   <head>
     <title>Home - Bloxcity</title>
   </head>
+  <body>
+    <form action="" method="POST">
+      <input type="text" name="username" placeholder="Enter your username...">
+      <button type="submit"></button>
+    </form>
+  </body>
 </html>
 <?php
 echo "Hello guys welcome to my application.";
